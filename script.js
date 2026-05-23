546bc5bb58b67f0bffd8db5c06f4902d461ac49e
shehunpt <shehuangelo7@gmail.com>
2026-05-23 18:28:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.querySelectorAll('.faq-item').forEach(item => {
+    const title = item.querySelector('.faq-title');
+    title.addEventListener('click', () => {
+        // Chiude tutti gli altri elementi
+        document.querySelectorAll('.faq-item').forEach(i => {
+            if (i !== item) i.classList.remove('aperto');
+        });
+        // Apre/chiude solo quello cliccato
+        item.classList.toggle('aperto');
+    });
+});
